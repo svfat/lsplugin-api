ffb9cc694913a84dd7c93dce5be397983bab7593
Stanislav Fateev <fateevstas@yandex.ru>
2014-05-14 18:22:10 +0400

    Push

--- classes/modules/api/modules/module.geo.class.php
+++ classes/modules/api/modules/module.geo.class.php
@@ -11,20 +11,20 @@ class PluginApi_ModuleApi_Geo extends PluginApi_ModuleApi_Module {
 	}
 
 	protected function ActionCountry() {
-		if (!$this->getParam('id')) {
+		if (!$iParamID=$this->getParam('id')) {
 			throw new ExceptionApiRequestError($this->Lang_Get('system_error'));
 		}
 	/*	$oTopic = $this->Topic_GetTopicById($this->getParam('id'));
 		if (!$oTopic) {
 			throw new ExceptionApiRequestError($this->Lang_Get('system_error'));
 		}*/
-		if (!($oCountry=$this->Geo_GetCountryById($this->getParam('id')))) {
+		if (!($oCountry=$this->Geo_GetCountryById($this->$iParamID))) {
 			return 'Country not found';
 		}
 		/*$aResult=$this->Geo_GetTargets(array('id'=>$oCountry->getId(),'target_type'=>'user'),$iPage,Config::Get('module.user.per_page'));
 		*/
 		$iCount=0;
-		$aResult=$this->Geo_GetTargets(array(getParam('id'),'target_type'=>'user'),1,9999);
+		$aResult=$this->Geo_GetTargets(array($iParamID,'target_type'=>'user'),1,9999);
 
 		$aUsersId=array();
 		foreach($aResult['collection'] as $oTarget) {
