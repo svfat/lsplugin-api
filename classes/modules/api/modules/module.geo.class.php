86a1099b0ed3b6ac7ba5754ad88f915f183d0278
Stanislav Fateev <fateevstas@yandex.ru>
2014-05-14 18:36:07 +0400

    Push

--- classes/modules/api/modules/module.geo.class.php
+++ classes/modules/api/modules/module.geo.class.php
@@ -32,6 +32,7 @@ class PluginApi_ModuleApi_Geo extends PluginApi_ModuleApi_Module {
 		$aUsersCountry=$this->User_GetUsersAdditionalData($aUsersId);
 		foreach($aUsersCountry as $oUser) {
 			echo $oUser->getProfileName();
+			echo ;
 		}
 		return $aUsersCountry;
 	}
