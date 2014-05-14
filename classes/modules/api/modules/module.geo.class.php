5aa7afa87fb772f46a22ab32e1713e4cfd0ad8c4
Stanislav Fateev <fateevstas@yandex.ru>
2014-05-14 18:27:40 +0400

    Push

--- classes/modules/api/modules/module.geo.class.php
+++ classes/modules/api/modules/module.geo.class.php
@@ -23,7 +23,7 @@ class PluginApi_ModuleApi_Geo extends PluginApi_ModuleApi_Module {
 		}
 		/*$aResult=$this->Geo_GetTargets(array('id'=>$oCountry->getId(),'target_type'=>'user'),$iPage,Config::Get('module.user.per_page'));
 		*/
-		$aResult=$this->Geo_GetTargets(array($iParamID,'target_type'=>'user'),1,9999);
+		$aResult=$this->Geo_GetTargets(array($iParamID,'target_type'=>'user'),0,30);
 
 		$aUsersId=array();
 		foreach($aResult['collection'] as $oTarget) {
