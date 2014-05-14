2357bcd0161b785057074bd9720a9d7df8d073d9
Stanislav Fateev <fateevstas@yandex.ru>
2014-05-14 20:58:55 +0400

    Push

--- classes/modules/api/modules/module.geo.class.php
+++ classes/modules/api/modules/module.geo.class.php
@@ -11,18 +11,13 @@ class PluginApi_ModuleApi_Geo extends PluginApi_ModuleApi_Module {
 	}
 
 	protected function ActionCountry() {
-		if (!($iParamID=$this->getParam('id'))) {
+		if (!($iParamId=$this->getParam('id'))) {
 			throw new ExceptionApiRequestError($this->Lang_Get('system_error'));
 		}
-	/*	$oTopic = $this->Topic_GetTopicById($this->getParam('id'));
-		if (!$oTopic) {
+		if (!($oCountry=$this->Geo_GetCountryById($iParamId))) {
 			throw new ExceptionApiRequestError($this->Lang_Get('system_error'));
-		}*/
-		if (!($oCountry=$this->Geo_GetCountryById($iParamID))) {
-			return 'Country not found';
 		}
-		/*$aResult=$this->Geo_GetTargets(array('id'=>$oCountry->getId(),'target_type'=>'user'),$iPage,Config::Get('module.user.per_page'));
-		*/
+
 
 		$aResult=$this->Geo_GetTargets(array('country_id'=>$oCountry->getId(),'target_type'=>'user'),1,30);
 		$aUsersId=array();
@@ -48,6 +43,77 @@ class PluginApi_ModuleApi_Geo extends PluginApi_ModuleApi_Module {
 
 		return array('collection'=>$aResult,'count'=>count($aResult));
 	}
+
+	protected function ActionRegion() {
+		if (!($iParamId=$this->getParam('id'))) {
+			throw new ExceptionApiRequestError($this->Lang_Get('system_error'));
+		}
+		if (!($oRegion=$this->Geo_GetRegionById($iParamId))) {
+			throw new ExceptionApiRequestError($this->Lang_Get('system_error'));
+		}
+
+
+		$aResult=$this->Geo_GetTargets(array('region_id'=>$oRegion->getId(),'target_type'=>'user'),1,30);
+		$aUsersId=array();
+		foreach($aResult['collection'] as $oTarget) {
+			$aUsersId[]=$oTarget->getTargetId();
+		}
+
+		
+		$aResult = array();
+		$aUsers=$this->User_GetUsersByArrayId($aUsersId);
+		foreach($aUsers as $k => $oUser) {
+			$aResult[$k]=array('login' => $oUser->getLogin(),
+							   'skill' => $oUser->getSkill(),
+							   'rating' => $oUser->getRating(),
+							   'name' => $oUser->getProfileName(),
+							   'country' => $oUser->getProfileCountry(),
+							   'region' => $oUser->getProfileRegion(),
+							   'city' => $oUser->getProfileCity(),
+							   'webpath' => $oUser->getUserWebPath()
+							   );
+		}
+
+
+		return array('collection'=>$aResult,'count'=>count($aResult));
+	}
+
+
+	protected function ActionCity() {
+		if (!($iParamId=$this->getParam('id'))) {
+			throw new ExceptionApiRequestError($this->Lang_Get('system_error'));
+		}
+		if (!($oCity=$this->Geo_GetCityById($iParamId)) {
+			throw new ExceptionApiRequestError($this->Lang_Get('system_error'));
+		}
+
+
+		$aResult=$this->Geo_GetTargets(array('city_id'=>$oCity->getId(),'target_type'=>'user'),1,30);
+		$aUsersId=array();
+		foreach($aResult['collection'] as $oTarget) {
+			$aUsersId[]=$oTarget->getTargetId();
+		}
+
+		
+		$aResult = array();
+		$aUsers=$this->User_GetUsersByArrayId($aUsersId);
+		foreach($aUsers as $k => $oUser) {
+			$aResult[$k]=array('login' => $oUser->getLogin(),
+							   'skill' => $oUser->getSkill(),
+							   'rating' => $oUser->getRating(),
+							   'name' => $oUser->getProfileName(),
+							   'country' => $oUser->getProfileCountry(),
+							   'region' => $oUser->getProfileRegion(),
+							   'city' => $oUser->getProfileCity(),
+							   'webpath' => $oUser->getUserWebPath()
+							   );
+		}
+
+
+		return array('collection'=>$aResult,'count'=>count($aResult));
+	}
+
+
 
 
 }
